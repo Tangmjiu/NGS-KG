272f29a37629bbce205c2b0d746369dd40069cc0
Tangmjiu <3502504526@qq.com>
2026-05-04 07:57:38 +0000

    Update API base URL to new server [skip ci]

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'NGS-KG+';
-  static const String baseUrl = 'https://kugouapi.mjiutang.qzz.io';
+  static const String baseUrl = 'http://171.80.2.129:42980';
   static const Duration connectTimeout = Duration(seconds: 15);
   static const Duration receiveTimeout = Duration(seconds: 15);
   static const String userAgent = 'Mozilla/5.0 (Linux; Android 14; NGS-KG+) AppleWebKit/537.36';
