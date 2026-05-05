5ca32cb3d6d38badeaf5e0c16e78c1804efa7260
Tangmjiu <3502504526@qq.com>
2026-05-05 12:15:09 +0000

    fix: store fileid map for instant unlike without API cache dependency

--- lib/providers/liked_songs_provider.dart
+++ lib/providers/liked_songs_provider.dart
@@ -4,6 +4,7 @@ import '../services/music_service.dart';
 class LikedSongsProvider extends ChangeNotifier {
   final MusicService _musicService = MusicService();
   final Set<int> _likedIds = {};
+  final Map<int, int> _fileidMap = {};
   bool _loaded = false;
 
   Set<int> get likedIds => _likedIds;
@@ -17,9 +18,13 @@ class LikedSongsProvider extends ChangeNotifier {
     try {
       final songs = await _musicService.getPlaylistTracksById(1);
       _likedIds.clear();
+      _fileidMap.clear();
       for (final s in songs) {
         final id = s['audio_id'] ?? s['id'] ?? 0;
-        _likedIds.add(id is int ? id : int.tryParse(id.toString()) ?? 0);
+        final aid = id is int ? id : int.tryParse(id.toString()) ?? 0;
+        _likedIds.add(aid);
+        final fid = s['fileid'];
+        if (fid != null) _fileidMap[aid] = fid is int ? fid : int.tryParse(fid.toString()) ?? 0;
       }
       _loaded = true;
       notifyListeners();
@@ -39,8 +44,18 @@ class LikedSongsProvider extends ChangeNotifier {
       final data = song.hash.isNotEmpty
           ? '${song.name}|${song.hash}|${song.albumId}|${song.audioId}'
           : song.name;
-      await _musicService.addTracksToPlaylist(1, data);
+      final res = await _musicService.addTracksToPlaylist(1, data);
       _likedIds.add(song.id);
+      final dataMap = res['data'] as Map?;
+      if (dataMap != null) {
+        final info = dataMap['info'] as List?;
+        if (info != null && info.isNotEmpty) {
+          final fid = (info[0] as Map)['fileid'];
+          if (fid != null) {
+            _fileidMap[song.id] = fid is int ? fid : int.tryParse(fid.toString()) ?? 0;
+          }
+        }
+      }
       notifyListeners();
       return true;
     } catch (_) {
@@ -50,16 +65,12 @@ class LikedSongsProvider extends ChangeNotifier {
 
   Future<bool> _unlike(SongInfo song) async {
     try {
-      final songs = await _musicService.getPlaylistTracksById(1);
-      final match = songs.firstWhere(
-        (s) => (s['audio_id'] ?? s['id']) == song.id,
-        orElse: () => <String, dynamic>{},
-      );
-      final fileid = match['fileid'];
+      final fileid = _fileidMap[song.id];
       if (fileid != null) {
         await _musicService.removeTracksFromPlaylist(1, fileid.toString());
       }
       _likedIds.remove(song.id);
+      _fileidMap.remove(song.id);
       notifyListeners();
       return true;
     } catch (_) {
