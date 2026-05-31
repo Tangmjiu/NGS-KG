fb458c95d1ee5c9832e969e389ff33fd2d400ba4
Tangmjiu <t@ngmjiu.com>
2026-05-31 16:53:18 +0800

    fix: album/songs和playlist/track/all恢复默认auth, 仅top/album保留noAuth

--- lib/repositories/album_repository.dart
+++ lib/repositories/album_repository.dart
@@ -24,7 +24,7 @@ class AlbumRepository extends BaseRepository {
       'page': page,
       'pagesize': pageSize,
     };
-    final res = await get('/album/songs', params: params, withAuth: false);
+    final res = await get('/album/songs', params: params);
     final data = res['data'];
     List<dynamic>? list;
     if (data is Map) {
